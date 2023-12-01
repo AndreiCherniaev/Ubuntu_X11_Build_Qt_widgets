b87d788d03250c3b2613ea2303546e437291b305
AndreiCherniaev <glscene2010@gmail.com>
2023-12-01 17:41:02 +0900

    fix path

--- example/readme.sh
+++ example/readme.sh
@@ -1,3 +1,3 @@
 "$MyBaseDir/Qt_themself/build_artifacts_host_powerfull/bin/qt-cmake" -S "$MyBaseDir/example/src" -B "$MyBaseDir/example/build-host" -DCMAKE_BUILD_TYPE=Release --fresh &&
-cmake --build build-host/ --parallel
+cmake --build "$MyBaseDir/example/build-host" --parallel
 "$MyBaseDir/example/build-host/myexample"
