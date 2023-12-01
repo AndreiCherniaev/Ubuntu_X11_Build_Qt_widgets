6b4c1ade76cd09e1e32c72515a0000ec93cb86a3
AndreiCherniaev <glscene2010@gmail.com>
2023-12-01 14:06:22 +0900

    fix ico

---
+++ Qt_themself/Uubntu_dependency.sh
@@ -0,0 +1,5 @@
+# host: Ubuntu 22
+sudo apt install libxcb-cursor0
+sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev
+sudo apt-get isntall libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxkbcommon-x11-0
+sudo apt install libxcb-xinerama0 
--- README.md
+++ README.md
@@ -14,5 +14,9 @@ Qt_themself/readme.sh
 
 # Build example
 ```
+echo $XDG_SESSION_TYPE
+export QT_DEBUG_PLUGINS=1
+# export QT_QPA_PLATFORM=linuxfb
+# export QT_QPA_EGLFS_FB=/dev/fb0
 example/readme.sh
 ```
