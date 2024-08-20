e2b088a823ee66e7adcdcd0f73fa5c6d2b32e0ec
AndreiCherniaev <glscene2010@gmail.com>
2024-08-20 11:05:04 +0900

    Update Ubuntu_dependency.sh

--- Qt_themself/Ubuntu_dependency.sh
+++ Qt_themself/Ubuntu_dependency.sh
@@ -5,6 +5,8 @@ set -e # in case of error in any line of this script do exit
 if [[ "$XDG_SESSION_TYPE" != "x11" ]]; then
         echo "war Please use x11"
 fi
+# Allow deb-src in sources.list
+sed -i 's/^Types: deb$/Types: deb deb-src/' /etc/apt/sources.list.d/ubuntu.sources # https://askubuntu.com/a/1512043/1087530
 
 sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
 # Thanks https://doc.qt.io/qt-6/linux-requirements.html
