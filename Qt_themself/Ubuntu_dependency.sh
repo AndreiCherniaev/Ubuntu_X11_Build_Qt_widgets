fad47390b2493d6e3de977cc675a17b9c9657346
AndreiCherniaev <glscene2010@gmail.com>
2024-08-20 11:10:40 +0900

    Update Ubuntu_dependency.sh

--- Qt_themself/Ubuntu_dependency.sh
+++ Qt_themself/Ubuntu_dependency.sh
@@ -13,7 +13,7 @@ sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-
 sudo apt install libfontconfig1-dev libfreetype6-dev libx11-dev libx11-xcb-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libxcb-cursor-dev libxcb-glx0-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev libxcb-util-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev -y
 
 sudo apt-get install libmd4c-html0 -y
-# Install cmake, ninja via 
+# Install cmake, ninja 
 sudo apt install python3-pip -y
 pip install cmake ninja --break-system-packages
 
