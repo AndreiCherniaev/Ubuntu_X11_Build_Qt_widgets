df15bccc705999a707e7d849246be88a93c2c7a5
AndreiCherniaev <dungeonlords789@yandex.ru>
2024-09-25 01:17:27 +0900

    Update readme.sh

--- Qt_themself/readme.sh
+++ Qt_themself/readme.sh
@@ -26,3 +26,6 @@ export QT_DEBUG_PLUGINS=1
 cd ${MyQtBaseDir}/build_host_powerfull
 ../qt5/configure -release -static -opensource -nomake examples -nomake tests -confirm-license -skip webengine -skip qtwayland -skip qtdoc -skip qtgraphicaleffects -skip qtqa -skip qttranslations -skip qtvirtualkeyboard -skip qtquicktimeline -skip qtquick3d -skip qt3d -skip qtrepotools -skip qttools -skip qtimageformats -skip qtnetworkauth -skip qtwebsockets -skip qtactiveqt -skip qtmacextras -skip winextras -skip qtmultimedia -skip qtgamepad -skip qtserialbus -skip qtspeech -skip qtsensors -skip qtlocation -no-ssl -prefix ../build_artifacts_host_powerfull -skip qtserialport -skip qtcharts -system-freetype -- -GNinja -DCMAKE_TOOLCHAIN_FILE=../toolchain_host_powerfull.cmake
 cmake --build . --parallel && cmake --install .
+
+# After Qt build successfully you can keep build_artifacts_host_powerfull only (remove source folder qt5/ and build_host_powerfull/)
+gio trash ${MyQtBaseDir}/qt5 ${MyQtBaseDir}/build_host_powerfull
